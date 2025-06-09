386c037700b1142cdf76ec7ea7aa69ba51d1b935
Sadia Hossain <sadiahossain2101@gmail.com>
2025-06-09 01:20:34 +0100

    Update 175-Combine Two Tables.sql

--- Easy/175-Combine Two Tables.sql
+++ Easy/175-Combine Two Tables.sql
@@ -1,8 +1,16 @@
--- Problem: Combine Two Tables
--- Difficulty: Easy
--- Link: https://leetcode.com/problems/combine-two-tables/
+--  Problem: Combine Two Tables
+--  https://leetcode.com/problems/combine-two-tables/
+--  Difficulty: Easy
 
-Select firstName, lastName, city, state
-From Person person
-Left Join Address address
-On person.personId = address.personId
+--  What this teaches:
+--   - LEFT JOIN: Get all records from Person, even if there’s no match in Address.
+--   - Joining using a shared key (personId).
+--   - Combining data from multiple tables safely.
+
+--  My Thought Process:
+--   - Use LEFT JOIN because not all persons might have an address.
+--   - Match on personId, then select the relevant columns.
+
+SELECT firstName, lastName, city, state
+FROM Person AS person
+LEFT JOIN Address AS address ON person.personId = address.personId;
